ce395866f492278050d248d40ed0a0a2453e8d7a
Chandan Kumar <chandanbkumar22@gmail.com>
2023-11-23 00:43:26 +0530

    Update Question20.java
    commented pattern

--- Question20.java
+++ Question20.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class Question20 {
     //pattern question
+    /*Enter Number:5
+            1
+            2 6
+            3 7 10
+            4 8 11 13
+            5 9 12 14 15*/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number:");
